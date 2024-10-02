77e8face761fe40e4c714c20d80d8d31ac751e78
Abdulfattahf <abdyaho545@gmail.com>
2024-10-03 00:29:35 +0300

    print your name

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void  printName(string name)
+{
+    cout<<"\n your name is :"<<name<<endl;
+
+}
+
+
+int main()
+{
+   printName("ABD ALFATTAH");
+    return 0;
+}
