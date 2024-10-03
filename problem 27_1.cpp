de997a4dddc179c9eb4b384ebd31d7182cbda8d9
Abdulfattahf <abdyaho545@gmail.com>
2024-10-03 23:16:54 +0300

    Print num of N to 1

---
+++ problem 27_1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+int Readnumber(int n){
+
+  cout<<"enter the number :"<<endl;
+  cin>>n;
+  return n;
+
+
+}
+
+void PrintFrom1ToN_UsingWhile(int n)
+{  int i=n;
+
+   cout<<"Range pranted using while statment :"<<endl;
+    while(i>=1)
+    {
+       cout<<i<<endl;
+        i--;
+    }
+
+
+}
+
+void  PrintFrom1ToN_UsingDo_While(int n)
+{  int i=n;
+
+   cout<<"Range pranted using dowhile statment :"<<endl;
+    do{
+        cout<<i<<endl;
+        i--;
+
+    }while(i>=1);
+
+}
+
+ void  PrintFrom1ToN_UsingFor(int n)
+ {
+
+     cout<<"Range pranted using For statment :"<<endl;
+     for(int i=n;i>=1;i--)
+     {
+
+         cout<<i<<endl;
+     }
+
+ }
+
+int main()
+{
+ int n=Readnumber(n);
+   PrintFrom1ToN_UsingWhile(n);
+  PrintFrom1ToN_UsingDo_While(n);
+     PrintFrom1ToN_UsingFor(n);
+    return 0;
+}
